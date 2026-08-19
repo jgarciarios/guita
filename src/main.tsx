c8d592ff412a7a53fb52a79bc9775d726e9700dc
Juan Ignacio García Ríos <113439528+jgarciarios@users.noreply.github.com>
2026-08-19 19:26:36 -0300

    Remove AuthGate from main.tsx
    Removed AuthGate component wrapping the App component.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
 import './design/global.css'
 import App from './App.tsx'
-import { AuthGate } from './components/AuthGate'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,9 +13,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <AuthGate>
-        <App />
-      </AuthGate>
+      <App />
     </ClerkProvider>
   </StrictMode>,
 )
